7887877855dd4ffbf4f87d6f550f2d3826509f40
Ronald Luna <43832306+RonaldLuna1@users.noreply.github.com>
2020-03-02 19:29:39 -0500

    Add files via upload

---
+++  P2T1_SalesPrediction_LunaRonald .py
@@ -0,0 +1,17 @@
+
+# This program projects the profit of the total sales. 
+# 2/19/2020
+# CTI-110 P2T1 - Sales Prediction
+# Ronald Luna 
+
+
+
+# Get the projected sale.
+total_sales = float (input('Enter the projected sales:'))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit
+print ('The profit is $',format (profit,',.2f'))
+
